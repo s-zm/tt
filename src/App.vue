427b0cfd403a81a2c0f4f268af66f4fcf47c254a
zm <zhengmopatty@163.com>
2022-05-07 15:30:44 +0800

    引入vant 组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-pinglun"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
     <router-view />
   </div>
 </template>
